8f71fec335cdc2bfb8e29e2599711eee0aed7316
floret <sorrowbedamned@gmail.com>
2013-05-29 18:31:47 +0200

    0=0=0=0=0=0

---
+++ BubbleSort/BubbleSort/BubbleSortPhilip/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace BubbleSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxSorted = new System.Windows.Forms.ListBox();
+            this.lbxUnsorted = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.txbInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbxSorted
+            // 
+            this.lbxSorted.FormattingEnabled = true;
+            this.lbxSorted.Location = new System.Drawing.Point(270, 12);
+            this.lbxSorted.Name = "lbxSorted";
+            this.lbxSorted.Size = new System.Drawing.Size(120, 264);
+            this.lbxSorted.TabIndex = 1;
+            // 
+            // lbxUnsorted
+            // 
+            this.lbxUnsorted.FormattingEnabled = true;
+            this.lbxUnsorted.Location = new System.Drawing.Point(12, 12);
+            this.lbxUnsorted.Name = "lbxUnsorted";
+            this.lbxUnsorted.Size = new System.Drawing.Size(120, 264);
+            this.lbxUnsorted.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(138, 83);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(126, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(138, 112);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(126, 23);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remove Selected";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(138, 141);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(126, 23);
+            this.btnSort.TabIndex = 5;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // txbInput
+            // 
+            this.txbInput.Location = new System.Drawing.Point(138, 57);
+            this.txbInput.Name = "txbInput";
+            this.txbInput.Size = new System.Drawing.Size(126, 20);
+            this.txbInput.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 283);
+            this.Controls.Add(this.txbInput);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lbxUnsorted);
+            this.Controls.Add(this.lbxSorted);
+            this.Name = "Form1";
+            this.Text = "Bubble Sort";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxSorted;
+        private System.Windows.Forms.ListBox lbxUnsorted;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.TextBox txbInput;
+    }
+}
+
---
+++ BubbleSort/BubbleSort/BubbleSortPhilip/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubbleSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lbxUnsorted.Items.Add(txbInput.Text);//Adds the contents of txbInput to lbxUnsorted
+            txbInput.Text = "";//clears the textbox.
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            lbxUnsorted.Items.Remove(lbxUnsorted.SelectedItem);//Removes the selected item in lbxUnsorted
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            int[] array = new int[lbxUnsorted.Items.Count];//creates new instance of the array called array.
+
+            for (int i = 0; i < lbxUnsorted.Items.Count; i++)//iterates for the lenght of 
+            {
+                object s = lbxUnsorted.Items[i];//sets s to the items in the unsorted listbox.
+                array[i] =Convert.ToInt32(s);//casts the objects to integers.
+            }
+            lbxSorted.Items.Clear();//clears the sorted listbox.
+            //sorting using bubble sort algorithim.
+            bool flag = true;//boolean value that indicates that the numbers are swapped or not.
+            int temp;
+            int numLength = array.Length;
+
+            for (int i = 1; (i <= (numLength - 1)) && flag; i++)//
+            {
+                flag = false;//sets the swapped indicator to false.
+                for (int j = 0; j < (numLength - 1); j++)
+                {
+                    if (array[j + 1] > array[j])//if the element after the current element is bigger than the current element.
+                    {
+                        temp = array[j];            //Swaps
+                        array[j] = array[j + 1];    //the 
+                        array[j + 1] = temp;        //elements
+                        flag = true;//sets the swapped indicator to true.
+                    }
+                }
+            }
+            foreach (int item in array)//adds the elements of the array one for one to the sorted listbox.
+            {
+                lbxSorted.Items.Add(item);//adds a single item to the listbox.
+            }
+        }
+    }
+}
